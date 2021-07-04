8dd72e54c0b699ab9c2bba68ca8d17cc7b95f28d
Gavin Epperson <gavinscottepperson@gmail.com>
2021-07-04 15:58:53 -0500

    All this for a toolbar -GE

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatToolbarModule
   ],
   imports: [
     BrowserModule,
