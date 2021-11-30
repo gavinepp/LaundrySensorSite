b47dff4a6d2590279da610d773e1d282f1001908
gavinepp <gavinscottepperson@gmail.com>
2021-11-30 06:40:52 -0600

    Revised the LaundyReceiver Code

--- Microcontroller-Code/LaundryReceiver/src/main.cpp
+++ Microcontroller-Code/LaundryReceiver/src/main.cpp
@@ -1,12 +1,13 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
+/*  
+  Gavin Epperson
+  UCA Honors Capstone, Fall 2021
   
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
+  WasherWatcher LaundryReceiver code
+  "main.cpp"
   
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
+  Code to be flashed onto each laundry room microcontroller (ESP32).
+  Data is received from multiple microcontroller senders, which is then used to update website files.
+  The receiver microcontroller hosts these website files for anyone locally to view.
 */
 
 #include <esp_now.h>
@@ -16,24 +17,25 @@
 #include "ESPAsyncWebServer.h"
 #include "SPIFFS.h"
 
-const char* SSID = "The High Cs";
-const char* PASSWORD = "curse of black pearl";
+const char* SSID = "UCAWIRELESS";
+const char* PASSWORD = "";
 
 AsyncWebServer server(80);
 AsyncEventSource events("/events");
 
-// Structure example to receive data
-// Must match the sender structure
+// Structure to receive data. Must match the sender structure
 typedef struct {
   char id[32];
   bool machineOn;
 } sensor_message;
 
-// Create a struct_message called myData
+// Create a sensor_message called receivedMessage
 sensor_message receivedMessage;
 
-// callback function that will be executed when data is received
+// Callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+
+  // Copy received data to global struct
   memcpy(&receivedMessage, incomingData, sizeof(receivedMessage));
 
   Serial.print("Sensor Name: ");
@@ -42,22 +44,30 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   Serial.println(receivedMessage.machineOn);
   Serial.println();
 
+  // Format the received data in JSON and send it as an AsyncEvent (handled by JavaScript)
   DynamicJsonDocument board(1024);
   board["id"] = receivedMessage.id;
   board["status"] = receivedMessage.machineOn;
+
   String jsonStr;
   serializeJson(board, jsonStr);
   events.send(jsonStr.c_str(), "machine_status", millis());
 }
 
-void initSPIFFS() {
-  if (!SPIFFS.begin()) {  Serial.println("An error has occurred while mounting SPIFFS"); }
-  else {  Serial.println("SPIFFS mounted successfully"); }
+// Helper function to initialize SPIFFS (SPI Flash File System), a way of storing files on the controller.
+bool initSPIFFS() {
+  if (!SPIFFS.begin()) {
+    Serial.println("An error has occurred while mounting SPIFFS");
+    return false;
+  }
+  Serial.println("SPIFFS mounted successfully");
+  return true;
 }
 
+// Helper function to connect the microcontroller to the WiFi network. Will hang indefinitely until it connects.
 void initWiFi() {
-  // Set device as a Wi-Fi Station AND access point
-  WiFi.mode(WIFI_AP_STA);
+  // Set device as a Wi-Fi station (able to serve the website to all users connected to the WiFi network)
+  WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, PASSWORD);
 
 
@@ -67,54 +77,66 @@ void initWiFi() {
     delay(1000);
   }
 
+  // Print information about the controller's WiFi connection
   Serial.print("\nStation IP Address: ");
   Serial.println(WiFi.localIP());
   Serial.print("Wi-Fi Channel: ");
   Serial.println(WiFi.channel());
+  Serial.print("WiFi MAC Address: ");
+  Serial.println(WiFi.macAddress());
 }
 
+/******************* Arduino Setup() Function ****************************
+ * Runs Arduino's built-in setup() function.
+ * Here, setup() initializes the WiFi connection, file system, and ESP-NOW
+ * and additionally prepares and begins hosting the WasherWatcher website.
+ **************************************************************************/
 void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
 
   initWiFi();
-  initSPIFFS();
-  
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
+
+  // Initialize ESP-NOW
+  if (initSPIFFS() != true) {
+    Serial.println("Error initializing SPIFFS. Returning from setup");
     return;
   }
   
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
+  // Initialize ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW. Returning from setup");
+    return;
+  }
+  
+  // Once ESP-NOW is successfully initialized, we will register our receiver handler
   esp_now_register_recv_cb(OnDataRecv);
 
-  // Handle Web Server
+  // Handle Web Server (direct users to the correct html when connecting)
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/index.html", "text/html");
   });
 
+  // Serve the website files stored in the "data" folder with SPIFFS
   server.serveStatic("/", SPIFFS, "/");
-   
+  
+  // Add AsyncEvent for when users connect
   events.onConnect([](AsyncEventSourceClient *client){
     if(client->lastId()){
       Serial.printf("Client reconnected! Last message ID received is: %u\n", client->lastId());
     }
-    // send event with message "hello!", id current millis
-    // and set reconnect delay to 1 second
+    // Send event with message "hello!", whose id is current millis and set reconnect delay to 1 second
     client->send("hello!", NULL, millis(), 10000);
   });
+
+  // Add the AsyncEvents to the WebServer and begin hosting the website 
   server.addHandler(&events);
   server.begin();
 }
  
-void loop() {
-  // Optional?
-  static unsigned long lastEventTime = millis();
-  static const unsigned long EVENT_INTERVAL_MS = 5000;
-  if ((millis() - lastEventTime) > EVENT_INTERVAL_MS) {
-    events.send("ping",NULL,millis());
-    lastEventTime = millis();
-  }
-}
+ /******************* Arduino Loop() Function ****************************
+ * Runs Arduino's built-in loop() function, which repeats indefinitely while the microcontroller is powered.
+ * Here, loop() is empty, as our AsyncEvent handlers run automatically when data
+ * is received, meaning we don't have to constantly poll for new data in the loop,
+ *************************************************************************/
+void loop() {}
